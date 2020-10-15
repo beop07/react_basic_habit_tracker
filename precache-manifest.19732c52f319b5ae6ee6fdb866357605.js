4a41fdfbd7626a194795e4d2b22ceef2c7114b6c
beop07 <beop07@naver.com>
2020-10-15 15:40:02 +0900

    Updates

---
+++ precache-manifest.19732c52f319b5ae6ee6fdb866357605.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f957ed0d59b6bcd665cd333fd480ae1",
+    "url": "/react_basic_habit_tracker/index.html"
+  },
+  {
+    "revision": "00ffe363dea93c789fc3",
+    "url": "/react_basic_habit_tracker/static/css/main.907db87d.chunk.css"
+  },
+  {
+    "revision": "cb24699852ce2fa9952c",
+    "url": "/react_basic_habit_tracker/static/js/2.f1f7639e.chunk.js"
+  },
+  {
+    "revision": "746582258d70325461391e08406982a5",
+    "url": "/react_basic_habit_tracker/static/js/2.f1f7639e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00ffe363dea93c789fc3",
+    "url": "/react_basic_habit_tracker/static/js/main.042542e8.chunk.js"
+  },
+  {
+    "revision": "f07e0338c5d24342834d",
+    "url": "/react_basic_habit_tracker/static/js/runtime-main.0134c98d.js"
+  }
+]);
